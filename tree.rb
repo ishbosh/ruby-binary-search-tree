faaf2e2ad9ccdb5f9857b98ad60aa98ab463c579
ishbosh <99460383+ishbosh@users.noreply.github.com>
2022-12-09 18:07:17 -0700

    Rename mid to mid_index in build_tree method for clarity

--- tree.rb
+++ tree.rb
@@ -12,12 +12,12 @@ class Tree
     # if the start point is greater than the end point, return nil
     return nil if start_index > end_index
     # Get the Middle data point (this is the level-0 root node)
-    mid = (start_index + end_index) / 2
-    root = Node.new(array[mid])
+    mid_index = (start_index + end_index) / 2
+    root = Node.new(array[mid_index])
     # set the left node equal to build_tree(array, start, mid-1)
-    root.left = build_tree(@array, start_index, mid - 1)
+    root.left = build_tree(@array, start_index, mid_index - 1)
     # set the right node equal to build_tree(array, mid+1, end)
-    root.right = build_tree(@array, mid + 1, end_index)
+    root.right = build_tree(@array, mid_index + 1, end_index)
     # Return the level-0 root node
     root
   end

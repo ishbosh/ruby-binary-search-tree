2b2f58489a4fe3c101ba170fe4e4d38d51dab115
ishbosh <99460383+ishbosh@users.noreply.github.com>
2022-12-10 11:07:47 -0700

    Remove pseudocode comments

--- tree.rb
+++ tree.rb
@@ -9,16 +9,12 @@ class Tree
   end
 
   def build_tree(array, start_index, end_index)
-    # if the start point is greater than the end point, return nil
     return nil if start_index > end_index
-    # Get the Middle data point (this is the level-0 root node)
+
     mid_index = (start_index + end_index) / 2
     root = Node.new(array[mid_index])
-    # set the left node equal to build_tree(array, start, mid-1)
     root.left = build_tree(array, start_index, mid_index - 1)
-    # set the right node equal to build_tree(array, mid+1, end)
     root.right = build_tree(array, mid_index + 1, end_index)
-    # Return the level-0 root node
     root
   end
 

6809c9ca1d11eccba3f6b34e5efde6ebd4bf043d
ishbosh <99460383+ishbosh@users.noreply.github.com>
2022-12-09 18:05:48 -0700

    Create first iteration of build_tree method

--- tree.rb
+++ tree.rb
@@ -1,14 +1,24 @@
+require_relative 'node'
+
 class Tree
   def initialize(array)
     @array = array.uniq.sort
-    @root = build_tree(array)
+    end_index = @array.length - 1
+    start_index = 0
+    @root = build_tree(@array, start_index, end_index)
   end
 
-  def build_tree(array, start, end)
+  def build_tree(array, start_index, end_index)
     # if the start point is greater than the end point, return nil
+    return nil if start_index > end_index
     # Get the Middle data point (this is the level-0 root node)
+    mid = (start_index + end_index) / 2
+    root = Node.new(array[mid])
     # set the left node equal to build_tree(array, start, mid-1)
+    root.left = build_tree(@array, start_index, mid - 1)
     # set the right node equal to build_tree(array, mid+1, end)
+    root.right = build_tree(@array, mid + 1, end_index)
     # Return the level-0 root node
+    root
   end
 end
